c35606f440dbee0ed9f789401a2baac40e308e40
wyyyyyy <301059120@qq.com>
2019-01-08 18:01:24 +0800

    2019-1-8

--- bicycle/src/router/about.js
+++ bicycle/src/router/about.js
@@ -1,11 +1,56 @@
 import React, {Component} from 'react';
 
-export default class About extends Component{
+function NoneState(props) {
+  console.log(props)
+  return(
+    <div>
+      <div>{props.children}</div>
+      <div>{props.left}</div>
+      <div>{props.right}</div>
+    </div>
+  )
+}
+
+class Abox extends Component{
   render() {
-    return(
-      <div>
-        about page
-      </div>
+    return (
+      <div>aaa</div>
     )
   }
 }
+
+class Bbox extends Component{
+  render() {
+    return (
+      <div>bbb</div>
+    )
+  }
+}
+
+function About () {
+  return (
+    <div>
+      <NoneState
+        left = {
+          <Abox/>
+        }
+        right = {
+          <Bbox/>
+        }
+      />
+      <NoneState>
+        <p>hello,world</p>
+      </NoneState>
+    </div>
+  )
+}
+
+export default About;
+
+
+
+
+
+
+
+
